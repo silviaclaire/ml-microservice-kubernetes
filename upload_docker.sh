0d10383c0f78fe0cf21ee8a6081ad34bb5085589
Claire Yan <silviaclaire@gmail.com>
2020-03-19 08:04:37 +0000

    update: upload_docker.sh

--- upload_docker.sh
+++ upload_docker.sh
@@ -3,13 +3,16 @@
 
 # Assumes that an image is built via `run_docker.sh`
 
-# Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+image="boston-housing-prediction"
+dockerpath="silviaclaire/"+$image
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+# docker login
+docker tag $image:lastest $dockerpath:lastest
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath:lastest
