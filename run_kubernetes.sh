84c4c9eef78dbfb6989d49ac1cdb8c8dd7603ccb
Claire Yan <silviaclaire@gmail.com>
2020-03-19 08:05:49 +0000

    update: run_kubernetes.sh

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -2,17 +2,19 @@
 
 # This tags and uploads an image to Docker Hub
 
-# Step 1:
-# This is your Docker ID/path
-# dockerpath=<>
+# Port that exposed from our service to host
+port=8000
 
-# Step 2
+# Create dockerpath
+image="boston-housing-prediction"
+dockerpath="silviaclaire/"+$image
+
 # Run the Docker Hub container with kubernetes
+kubectl create deployment $image --image=$dockerpath:v1
 
-
-# Step 3:
 # List kubernetes pods
+kubectl get pods
 
-# Step 4:
 # Forward the container port to a host
-
+kubectl expose deployment/$image --type="NodePort" --port $port
+service/$image exposed
