b014a35989132e532ede7a5304d7fcc00eebd810
silviaclaire <silviaclaire@gmail.com>
2020-03-20 01:39:40 +0900

    fix: forward service port 80 to host as 8000

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -3,6 +3,7 @@
 # This tags and uploads an image to Docker Hub
 
 # Port that exposed from our service to host
+internal_port=80
 external_port=8000
 
 # Create dockerpath
@@ -11,9 +12,10 @@ dockerpath="silviaclaire/$image"
 
 # Run the Docker Hub container with kubernetes
 kubectl create deployment $image --image=$dockerpath:latest
+kubectl expose deployment/$image --type="NodePort" --port $internal_port
 
 # List kubernetes pods
 kubectl get pods
 
 # Forward the container port to a host
-kubectl expose deployment/$image --type="NodePort" --port $external_port
+kubectl port-forward services/$image $external_port:$internal_port
